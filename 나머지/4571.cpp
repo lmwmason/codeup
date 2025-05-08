7bf22468e01489f5af454d66f64c1e5dcb499d82
이무원 <149868756+lmwmason@users.noreply.github.com>
2025-05-08 19:16:54 +0900

    Create 4571.cpp

---
+++ 나머지/4571.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int M,N;
+int sum, min_;
+
+int main()
+{
+    min_ = 10000;
+    cin>>N>>M;
+    for(int i=N; i<=M; i++)
+    {
+        int a = sqrt(i);
+        if(sqrt(i)==a)
+        {
+            sum+=i;
+            if(min_>i) min_ = i;
+        }
+    }
+    if(sum==0) cout<<-1;
+    else cout<<sum<<"\n"<<min_;
+}
